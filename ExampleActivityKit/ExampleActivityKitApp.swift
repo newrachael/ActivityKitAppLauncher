00d0757e053a4d61c249a5004724eac8dcc76411
ray <ray@jejememe.com>
2022-12-29 01:31:43 +0900

    Initial Commit

---
+++ ExampleActivityKit/ExampleActivityKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleActivityKitApp.swift
+//  ExampleActivityKit
+//
+//  Created by raykim on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct ExampleActivityKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
